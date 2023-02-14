a2fb2322969981b1efba65ca744e549ef78ce7f2
nstonic <118763806+nstonic@users.noreply.github.com>
2023-02-14 22:37:59 +0400

    Переименовал исполнителя

--- markups.py
+++ markups.py
@@ -2,6 +2,6 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 choose_roll = InlineKeyboardMarkup(row_width=2)
 btn_quiz = InlineKeyboardButton(text="Заказчик", callback_data="roll_client")
-btn_info = InlineKeyboardButton(text="Исполнитель", callback_data="roll_doer")
+btn_info = InlineKeyboardButton(text="Исполнитель", callback_data="roll_freelancer")
 choose_roll.add(btn_quiz)
 choose_roll.add(btn_info)
