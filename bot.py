17469a0539ccee4092ec10083ef3946ffb05c34d
nstonic <nstonic@gmail.com>
2023-02-15 09:26:49 +0400

    Fix types and update registations

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import telebot
 from dotenv import load_dotenv
 
+import db_client
 import markups
 import messages
 
@@ -11,19 +12,19 @@ bot = telebot.TeleBot(os.environ['BOT_TOKEN'], parse_mode=None)
 
 
 @bot.message_handler(commands=['find_orders'])
-def show_orders(message):
+def show_orders(message: telebot.types.Message):
     """Выводим список 5 случайных свободных заказов"""
     pass
 
 
 @bot.message_handler(commands=['new_order'])
-def show_orders(message):
+def show_orders(message: telebot.types.Message):
     """Создаем новый заказ"""
     pass
 
 
 @bot.message_handler(commands=['my_orders'])
-def show_orders(message):
+def show_orders(message: telebot.types.Message):
     """Выводим список заказов"""
     bot.reply_to(message,
                  messages.MY_ORDERS,
@@ -31,7 +32,7 @@ def show_orders(message):
 
 
 @bot.message_handler(commands=['register'])
-def send_welcome(message):
+def send_welcome(message: telebot.types.Message):
     """Предлагаем зарегистрироваться в роли исполнителя или заказчика"""
     bot.reply_to(message,
                  messages.REGISTER,
@@ -39,28 +40,30 @@ def send_welcome(message):
 
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
+def send_welcome(message: telebot.types.Message):
     """Выводим приветствие и список возможны запросов"""
     bot.reply_to(message,
                  messages.HELP.format(message.chat.first_name))
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'roll_client')
-def register_client(call):
+def register_client(message: telebot.types.Message):
     """Регистрируем клиента в базе"""
-    bot.answer_callback_query(call.id, text=messages.REGISTER_OK)
+    if db_client.register_client(message.from_user.id):
+        bot.answer_callback_query(message.id, text=messages.REGISTER_OK)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'roll_freelancer')
-def register_client(call):
+def register_freelancer(message: telebot.types.Message):
     """Регистрируем заказчика в базе"""
-    bot.answer_callback_query(call.id, text=messages.REGISTER_OK)
+    if db_client.register_freelancer(message.from_user.id):
+        bot.answer_callback_query(message.id, text=messages.REGISTER_OK)
 
 
-@bot.callback_query_handler(func=lambda call: 'order_' in call.data)
-def register_client(call):
+@bot.callback_query_handler(func=lambda call: True, regexp=r'order_\d+')
+def show_order_info(message: telebot.types.Message):
     """Отображаем информацию по заказу"""
-    bot.answer_callback_query(call.id, text='ваш заказ')
+    bot.answer_callback_query(message.id, text='ваш заказ')
 
 
 if __name__ == '__main__':
