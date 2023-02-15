171412a032dfc8cb608f539ff3c311978745227a
dmitriev-ilya <dmitriev-ilya@inbox.ru>
2023-02-15 22:20:53 +0500

    models init

---
+++ models.py
@@ -0,0 +1,66 @@
+import os
+import datetime
+
+from dotenv import load_dotenv
+from peewee import *
+
+load_dotenv()
+
+db = SqliteDatabase(os.getenv('DATABASE_PATH'))
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Client(BaseModel):
+    telegram_id = CharField()
+    registered_at = DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return f'id: {self.telegram_id}, registred: {self.registered_at}'
+
+
+class Freelancer(BaseModel):
+    telegram_id = CharField()
+    access = BooleanField()
+    registered_at = DateTimeField(default=datetime.datetime.now)
+
+
+class Subscription(BaseModel):
+    client = ForeignKeyField(Client, backref='subscriptions')
+    started_at = DateTimeField(default=datetime.datetime.now)
+    duration = IntegerField(null=False)
+
+
+class Ticket(BaseModel):
+    client = ForeignKeyField(Client, backref='tickets')  
+    text = TextField()
+    rate = DecimalField()
+
+
+class Order(BaseModel):
+    ticket = ForeignKeyField(Ticket)
+    freelancer = ForeignKeyField(Freelancer, backref='orders')
+    estimate_time = CharField(max_length=100)
+    started_at = DateTimeField(default=datetime.datetime.now)
+    completed_at = DateTimeField(null=True)
+    status = CharField(max_length=100)
+
+
+class Message(BaseModel):
+    order = ForeignKeyField(Order, backref='messages')
+    telegram_id = CharField()
+    text = TextField()
+    sending_at = DateTimeField(default=datetime.datetime.now)
+
+
+def create_tables():
+    with db:
+        db.create_tables([Client, Freelancer, Subscription, Ticket, Order, Message])
+
+
+if __name__ == '__main__':
+    create_tables()
+    print('Schema created')
