0c80af4165d94306bcbade80ac5846d00cf70a0c
nstonic <nstonic@gmail.com>
2023-02-15 09:49:23 +0400

    Add new_order and find_orders
    Пока не работает

--- bot.py
+++ bot.py
@@ -11,20 +11,28 @@ load_dotenv()
 bot = telebot.TeleBot(os.environ['BOT_TOKEN'], parse_mode=None)
 
 
-@bot.message_handler(commands=['find_orders'])
-def show_orders(message: telebot.types.Message):
+@bot.message_handler(commands=['find_order'])
+def find_orders(message: telebot.types.Message):
     """Выводим список 5 случайных свободных заказов"""
-    pass
+    match db_client.who_is_it(message.from_user.id):
+        case 'freelancer':
+            pass
+        case 'client' | None:
+            bot.answer_callback_query(message.id, text=messages.ONLY_FREELANCER_CAN_SEARCH_FOR_ORDERS)
 
 
 @bot.message_handler(commands=['new_order'])
-def show_orders(message: telebot.types.Message):
+def create_new_order(message: telebot.types.Message):
     """Создаем новый заказ"""
-    pass
+    match db_client.who_is_it(message.from_user.id):
+        case 'client':
+            pass
+        case 'freelancer' | None:
+            bot.answer_callback_query(message.id, text=messages.ONLY_CLIENT_CAN_CREATE_ORDER)
 
 
 @bot.message_handler(commands=['my_orders'])
-def show_orders(message: telebot.types.Message):
+def show_user_orders(message: telebot.types.Message):
     """Выводим список заказов"""
     bot.reply_to(message,
                  messages.MY_ORDERS,
@@ -32,7 +40,7 @@ def show_orders(message: telebot.types.Message):
 
 
 @bot.message_handler(commands=['register'])
-def send_welcome(message: telebot.types.Message):
+def register(message: telebot.types.Message):
     """Предлагаем зарегистрироваться в роли исполнителя или заказчика"""
     bot.reply_to(message,
                  messages.REGISTER,
