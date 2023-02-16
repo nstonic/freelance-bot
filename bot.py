e456da5de89dd1d17eab95cb0d33eed46a83594b
nstonic <118763806+nstonic@users.noreply.github.com>
2023-02-16 22:11:19 +0400

    adding show ticket info

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 import telebot
 from dotenv import load_dotenv
@@ -16,10 +15,7 @@ bot = telebot.TeleBot(os.environ['BOT_TOKEN'], parse_mode=None)
 @bot.message_handler(commands=['start'])
 def start(message: telebot.types.Message):
     """Выводим приветствие и предложение зарегистрироваться"""
-    try:
-        bot.delete_message(chat_id=message.chat.id, message_id=message.id)
-    except telebot.apihelper.ApiTelegramException:
-        pass
+    delete_messages(chat_id=message.chat.id, mes_ids=[message.id])
     if not db_client.who_is_it(message.from_user.id):
         bot.send_message(message.chat.id,
                          messages.START.format(message.chat.first_name),
@@ -31,16 +27,16 @@ def start(message: telebot.types.Message):
 @bot.callback_query_handler(func=lambda call: call.data == 'register')
 def register(call: telebot.types.CallbackQuery):
     """Предлагаем зарегистрироваться в роли исполнителя или заказчика"""
-    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.id)
+    delete_messages(chat_id=call.message.chat.id, mes_ids=[call.message.id])
     bot.send_message(call.message.chat.id,
-                     messages.REGISTER,
+                     messages.CHOOSE_ROLL,
                      reply_markup=markups.choose_roll())
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'help')
 def show_help(call: telebot.types.CallbackQuery):
     """Показываем справку"""
-    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.id)
+    delete_messages(chat_id=call.message.chat.id, mes_ids=[call.message.id])
     bot.send_message(call.message.chat.id,
                      messages.HELP,
                      reply_markup=markups.get_start_buttons())
@@ -51,7 +47,7 @@ def register_client(call: telebot.types.CallbackQuery):
     """Регистрируем клиента в базе"""
     if db_client.register_client(call.from_user.id):
         bot.answer_callback_query(call.id, text=messages.REGISTER_OK)
-    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.id)
+    delete_messages(chat_id=call.message.chat.id, mes_ids=[call.message.id])
     show_main_menu(call.message)
 
 
@@ -60,7 +56,7 @@ def register_freelancer(call: telebot.types.CallbackQuery):
     """Регистрируем заказчика в базе"""
     if db_client.register_freelancer(call.from_user.id):
         bot.answer_callback_query(call.id, text=messages.REGISTER_OK)
-    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.id)
+    delete_messages(chat_id=call.message.chat.id, mes_ids=[call.message.id])
     show_main_menu(call.message)
 
 
@@ -68,27 +64,30 @@ def register_freelancer(call: telebot.types.CallbackQuery):
 def show_main_menu(message: telebot.types.Message):
     """Выводим основное меню"""
 
-    user = db_client.who_is_it(message.chat.id)
-    text = messages.HELLO.format(message.from_user.first_name)
-    if user == 'client':
-        bot.send_message(message.chat.id,
-                         reply_markup=markups.get_client_menu(),
-                         text=text)
-    elif user == 'freelancer':
-        bot.send_message(message.chat.id,
-                         reply_markup=markups.get_freelancer_menu(),
-                         text=text)
+    user_roll = db_client.who_is_it(message.chat.id)
+    text = messages.LETS_WORK.format(message.from_user.first_name)
+    if user_roll == 'client':
+        markup = markups.get_client_menu()
+    elif user_roll == 'freelancer':
+        markup = markups.get_freelancer_menu()
+    else:
+        text = messages.MENU_IS_NOT_ALLOWED
+        markup = None
+    bot.send_message(message.chat.id,
+                     reply_markup=markup,
+                     text=text)
+    delete_messages(chat_id=message.chat.id, mes_ids=[message.id])
 
 
 @bot.message_handler(regexp='Найти заказ')
 def find_orders(message: telebot.types.Message):
     """Выводим список 5 случайных свободных заказов"""
-    bot.delete_message(chat_id=message.chat.id, message_id=message.id)
+    delete_messages(chat_id=message.chat.id, mes_ids=[message.id])
     user = db_client.who_is_it(message.from_user.id)
     if user == 'freelancer':
         pass
     elif user == 'client':
-        bot.send_message(message.chat.id, text=messages.ONLY_FREELANCER_CAN_SEARCH_FOR_ORDERS)
+        bot.send_message(message.chat.id, text=messages.SEARCHING_IS_NOT_ALLOWED)
     else:
         start(message)
 
@@ -96,7 +95,7 @@ def find_orders(message: telebot.types.Message):
 @bot.message_handler(regexp='Создать тикет')
 def create_new_ticket(message: telebot.types.Message):
     """Создаем новый тикет"""
-    bot.delete_message(chat_id=message.chat.id, message_id=message.id)
+    delete_messages(chat_id=message.chat.id, mes_ids=[message.id])
     user = db_client.who_is_it(message.from_user.id)
     if user == 'client':
         bot_message_id = bot.send_message(message.chat.id, text=messages.INPUT_TITLE).id
@@ -104,7 +103,7 @@ def create_new_ticket(message: telebot.types.Message):
                                        get_title,
                                        bot_message_id=bot_message_id)
     elif user == 'freelancer':
-        bot.send_message(message.chat.id, text=messages.ONLY_CLIENT_CAN_CREATE_ORDER)
+        bot.send_message(message.chat.id, text=messages.CREATING_IS_NOT_ALLOWED)
     else:
         start(message)
 
@@ -114,8 +113,7 @@ def get_title(message: telebot.types.Message, bot_message_id: int):
     ticket = dict(
         title=message.text
     )
-    bot.delete_message(chat_id=message.chat.id, message_id=bot_message_id)
-    bot.delete_message(chat_id=message.chat.id, message_id=message.id)
+    delete_messages(chat_id=message.chat.id, mes_ids=[message.id, bot_message_id])
     bot_message_id = bot.send_message(message.chat.id, text=messages.INPUT_TICKET_TEXT).id
     bot.register_next_step_handler(message,
                                    get_text,
@@ -126,8 +124,7 @@ def get_title(message: telebot.types.Message, bot_message_id: int):
 def get_text(message: telebot.types.Message, ticket: dict, bot_message_id: int):
     """Получаем от клиента текст тикета"""
     ticket['text'] = message.text
-    bot.delete_message(chat_id=message.chat.id, message_id=message.id)
-    bot.delete_message(chat_id=message.chat.id, message_id=bot_message_id)
+    delete_messages(chat_id=message.chat.id, mes_ids=[message.id, bot_message_id])
     bot_message_id = bot.send_message(message.chat.id, text=messages.INPUT_TICKET_RATE).id
     bot.register_next_step_handler(message,
                                    get_rate,
@@ -138,16 +135,17 @@ def get_text(message: telebot.types.Message, ticket: dict, bot_message_id: int):
 def get_rate(message: telebot.types.Message, ticket: dict, bot_message_id: int):
     """Получаем от клиента стоимость работ по тикету"""
     ticket['rate'] = message.text
-    bot.delete_message(chat_id=message.chat.id, message_id=message.id)
-    bot.delete_message(chat_id=message.chat.id, message_id=bot_message_id)
+    delete_messages(chat_id=message.chat.id, mes_ids=[message.id, bot_message_id])
     bot.send_message(message.chat.id,
-                     text=messages.TICKET_CREATED.format(ticket['title'], ticket['text'], ticket['rate']))
+                     text=messages.TICKET_CREATED.format(ticket['title'], ticket['text'], ticket['rate']),
+                     parse_mode='HTML')
+    show_main_menu(message)
 
 
 @bot.message_handler(regexp='Мои тикеты')
 def show_client_tickets(message: telebot.types.Message):
     """Выводим список тикетов клиента"""
-    bot.delete_message(chat_id=message.chat.id, message_id=message.id)
+    delete_messages(chat_id=message.chat.id, mes_ids=[message.id])
     bot.send_message(message.chat.id,
                      messages.MY_TICKETS,
                      reply_markup=markups.get_tickets_list())
@@ -156,14 +154,18 @@ def show_client_tickets(message: telebot.types.Message):
 @bot.callback_query_handler(func=lambda call: call.data.startswith('ticket_'))
 def show_ticket_info(call: telebot.types.CallbackQuery):
     """Отображаем информацию по тикету"""
+    order = db_client.show_ticket(int(call.data.strip('ticket_')))
     bot.answer_callback_query(call.id, text='ваш тикет')
-    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.id)
+    bot.send_message(chat_id=call.message.chat.id,
+                     text=messages.TICKET_INFO.format(**order),
+                     parse_mode='HTML')
+    delete_messages(chat_id=call.message.chat.id, mes_ids=[call.message.id])
 
 
 @bot.message_handler(regexp='Заказы в работе')
 def show_freelancer_orders(message: telebot.types.Message):
     """Выводим список заказов фрилансера"""
-    bot.delete_message(chat_id=message.chat.id, message_id=message.id)
+    delete_messages(chat_id=message.chat.id, mes_ids=[message.id])
     bot.send_message(message.chat.id,
                      messages.MY_ORDERS,
                      reply_markup=markups.get_orders_list())
@@ -173,7 +175,15 @@ def show_freelancer_orders(message: telebot.types.Message):
 def show_order_info(call: telebot.types.CallbackQuery):
     """Отображаем информацию по заказу"""
     bot.answer_callback_query(call.id, text='ваш заказ')
-    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.id)
+    delete_messages(chat_id=call.message.chat.id, mes_ids=[call.message.id])
+
+
+def delete_messages(chat_id: int, mes_ids: list):
+    for mes_id in mes_ids:
+        try:
+            bot.delete_message(chat_id=chat_id, message_id=mes_id)
+        except telebot.apihelper.ApiTelegramException:
+            pass
 
 
 if __name__ == '__main__':
