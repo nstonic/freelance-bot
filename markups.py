7a83cd5a10c6508a77ee8284f2e2c2584ef016d3
nstonic <118763806+nstonic@users.noreply.github.com>
2023-02-18 00:29:08 +0400

    Add get_back_main_menu

--- markups.py
+++ markups.py
@@ -3,22 +3,26 @@ from telebot import types
 import models
 
 
-def make_inline_markups_from_dict(btns: dict) -> types.InlineKeyboardMarkup:
+def make_inline_markups_from_dict(btns: dict, row_width: int = 2) -> types.InlineKeyboardMarkup:
     """Создает InlineKeyboardMarkup из словаря 'текст_кнопки':'callback_data' """
-    markup = types.InlineKeyboardMarkup(row_width=2)
+    markup = types.InlineKeyboardMarkup(row_width=row_width)
     buttons = [types.InlineKeyboardButton(text=button, callback_data=callback)
                for button, callback in btns.items()]
     markup.add(*buttons)
     return markup
 
 
-def make_menu_from_list(buttons_title: list) -> types.ReplyKeyboardMarkup:
+def make_menu_from_list(buttons_title: list, row_width: int = 2) -> types.ReplyKeyboardMarkup:
     """Создаёт меню из списка кнопок"""
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = [types.KeyboardButton(button)
                for button in buttons_title]
-    markup.add(*buttons)
+    markup.add(*buttons, row_width=row_width)
     return markup
+
+
+def get_back_main_menu() -> types.ReplyKeyboardMarkup:
+    return make_menu_from_list(['Назад', 'Основное меню'])
 
 
 def get_orders_list(orders: list[models.Order]) -> types.InlineKeyboardMarkup:
@@ -32,7 +36,7 @@ def get_orders_list(orders: list[models.Order]) -> types.InlineKeyboardMarkup:
     return my_orders_markup
 
 
-def get_tickets_list(tickets: list[models.Ticket]) -> types.InlineKeyboardMarkup:
+def get_tickets_list_inline(tickets: list[models.Ticket]) -> types.InlineKeyboardMarkup:
     """Создает кнопки для выбора тикета"""
     tickets_choose_markup = types.InlineKeyboardMarkup(row_width=2)
     buttons = [types.InlineKeyboardButton(
